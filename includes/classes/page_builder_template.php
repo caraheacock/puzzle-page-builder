778950b563924a3f738b07aca69e5d6537574711
Cara Heacock <caraheacock@gmail.com>
2016-12-06 21:13:22 -0600

    Fixes template functionality to be compatible with WordPress 4.7

--- includes/classes/page_builder_template.php
+++ includes/classes/page_builder_template.php
@@ -43,14 +43,9 @@ class PuzzlePageBuilderTemplate {
         $this->settings = new PuzzleSettings;
 
         /*
-         * Add a filter to insert template into the attributes metabox.
+         * Add a filter to insert template into the theme templates.
          */
-        add_filter('page_attributes_dropdown_pages_args', array($this, 'register_puzzle_page_builder_template'));
-        
-        /*
-         * Add a filter to insert our template into the post data.
-         */
-        add_filter('wp_insert_post_data', array($this, 'register_puzzle_page_builder_template'));
+        add_filter('theme_page_templates', array($this, 'register_puzzle_page_builder_template'));
         
         /*
          * Add a filter to the template include to determine if the page has our
@@ -65,39 +60,10 @@ class PuzzlePageBuilderTemplate {
         );
     }
     
-    /*
-     * Adds our template to the pages cache in order to trick WordPress
-     * into thinking the template file exists where it doesn't really exist.
-     */
-    public function register_puzzle_page_builder_template($atts) {
-        /* Create the key used for the themes cache */
-        $cache_key = 'page_templates-' . md5(get_theme_root() . '/' . get_stylesheet());
-        
-        /*
-         * Retrieve the cache list.
-         * If it doesn't exist, or if it's empty, prepare an array.
-         */
-        $templates = wp_get_theme()->get_page_templates();
-        if (empty($templates)) {
-            $templates = array();
-        }
-
-        /* New cache, therefore remove the old one */
-        wp_cache_delete($cache_key , 'themes');
-        
-        /*
-         * Now add our template to the list of templates by merging our templates
-         * with the existing templates array from the cache.
-         */
-        $templates = array_merge($templates, $this->templates);
-        
-        /*
-         * Add the modified cache to allow WordPress to pick it up for listing
-         * available templates
-         */
-        wp_cache_add($cache_key, $templates, 'themes', 1800);
-
-        return $atts;
+    /* Adds our template to the theme templates */
+    public function register_puzzle_page_builder_template($post_templates) {
+        $post_templates = array_merge($post_templates, $this->templates);
+        return $post_templates;
     }
     
     /*
@@ -122,9 +88,9 @@ class PuzzlePageBuilderTemplate {
         if (!isset($this->templates[$active_template])) return $template;
         
         /*
-         * Either hook into 'the_content' if the user wants to use their
-         * theme's templates, use a specific template that the user set, or
-         * use our own.
+         * Either use our own template, use a specific template that the user
+         * set, or hook into 'the_content' if the user wants to use their
+         * theme's templates
          */
         switch ($this->settings->display_sections_in()) {
             case 'plugin_template':
@@ -134,21 +100,22 @@ class PuzzlePageBuilderTemplate {
                 return get_theme_directory_uri() . $this->settings->custom_template();
                 break;
             default:
-                add_filter('the_content', 'ppb_the_content');
+                add_filter('the_content', array($this, 'puzzle_page_builder_content'));
                 return $template;
         }
         
     }
+    
+    /* Adds page builder content into 'the_content' */
+    function puzzle_page_builder_content($content) {
+        ob_start();
+        require(PPB_PLUGIN_DIR . '/views/partials/sections.php');
+        $content = ob_get_clean();
+    
+        return $content;
+    }
 }
 
 $puzzle_page_builder_template = new PuzzlePageBuilderTemplate;
-
-function ppb_the_content($content) {
-    ob_start();
-    require(PPB_PLUGIN_DIR . '/views/partials/sections.php');
-    $content = ob_get_clean();
-    
-    return $content;
-}
 
 ?>
