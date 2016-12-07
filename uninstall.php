b5864b5921fd269c59251932f98f31721921d6cb
Cara Heacock <caraheacock@gmail.com>
2016-12-07 01:19:57 -0600

    Adds uninstall clean-up, fixes #19

---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Puzzle Page Builder
+ * Uninstall
+ */
+
+if (!defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN')) exit();
+
+delete_post_meta_by_key('_puzzle_custom_template');
+delete_post_meta_by_key('_puzzle_page_sections');
+
+?>
